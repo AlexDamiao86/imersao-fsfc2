bf00aff2a678d026e76016791a90f43a3c8e2aff
AlexDamiao86 <alex_maiabr@hotmail.com>
2021-04-07 23:11:19 -0300

    Acrescenta testes em main.go

--- simulator/main.go
+++ simulator/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	kafka2 "github.com/codeedu/imersaofsfc2-simulator/application/kafka"
 	"github.com/codeedu/imersaofsfc2-simulator/infra/kafka"
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/joho/godotenv"
-	"log"
 )
 
 func init() {
@@ -17,6 +18,11 @@ func init() {
 }
 
 func main() {
+	// producer := kafka.NewKafkaProducer()
+	// kafka.Publish("Hello", "test", producer)
+	// for {
+	// 	_ = 1
+	// }
 	msgChan := make(chan *ckafka.Message)
 	consumer := kafka.NewKafkaConsumer(msgChan)
 	go consumer.Consume()
